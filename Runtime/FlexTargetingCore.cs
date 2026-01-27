ce04f349ec30f5fde9acb9d35fbdb1aebf1c0b71
clicona <46828592+c-licona@users.noreply.github.com>
2026-01-26 19:18:40 -0800

    Updated core methods documentation
    - Changed DetermineBestTargets to return the number of targets added to the output collection rather than the collections size itself

--- Runtime/FlexTargetingCore.cs
+++ Runtime/FlexTargetingCore.cs
@@ -100,12 +100,20 @@ namespace Cyclic.FlexTargeting
         }
 
         /// <summary>
-        /// Determines the best target of type <typeparamref name="TTarget"/> from the <paramref name="inputTargets"/> and
-        /// outputs that best target to <paramref name="bestTarget"/>. The data and scoring method that is used to
-        /// determine the best target is contained within the provided <paramref name="data"/>. An optional
-        /// <paramref name="targetFilter"/> can be provided to further filter out valid targets.
+        /// <para>
+        /// Filters and scores the targets in the <paramref name="inputTargets"/> list and outputs the best target to
+        /// <paramref name="bestTarget"/>. The data and scoring method that is used to determine the best target is
+        /// contained within the provided <paramref name="data"/>. An optional <paramref name="targetFilter"/> can be
+        /// provided to further filter out valid targets.
+        /// </para>
+        /// <remarks>
+        /// When using a <paramref name="targetFilter"/> it is important to be aware of what does and doesn't allocate
+        /// memory on the heap when working with delegates. Use the overload of this method that includes a context
+        /// object to properly use an instance or static method without allocating memory. See this link for more info:
+        /// https://github.com/KyryloKuzyk/PrimeTween?tab=readme-ov-file#zero-allocations-with-delegates
+        /// </remarks>
         /// </summary>
-        /// <param name="data">A container with the data and scoring method to use for sorting and ultimately
+        /// <param name="data">A structure with the data and scoring method to use for sorting and ultimately
         ///     determining the best target to return.</param>
         /// <param name="bestTarget">The best target that scores the best among the rest of the potential targets.
         ///     Will be null when no valid target was found.</param>
@@ -178,18 +186,17 @@ namespace Cyclic.FlexTargeting
 
         /// <summary>
         /// <para>
-        /// Determines the best target of type <typeparamref name="TTarget"/> from the <paramref name="inputTargets"/>
-        /// container and outputs that best target to <paramref name="bestTarget"/>. The data and scoring method that
-        /// is used to determine the best target is contained within the provided <paramref name="data"/>. An optional
-        /// <paramref name="targetFilter"/> can be provided to further filter out valid targets.
+        /// Filters and scores the targets in the <paramref name="inputTargets"/> list and outputs the best target to
+        /// <paramref name="bestTarget"/>. The data and scoring method that is used to determine the best target is
+        /// contained within the provided <paramref name="data"/>. An optional <paramref name="targetFilter"/> can be
+        /// provided to further filter out valid targets.
         /// </para>
-        /// <para>
-        /// If <paramref name="targetFilter"/> is an instance method, then calling it from the callback will implicitly
-        /// capture <c>this</c> reference, allocating heap memory. By passing in a <paramref name="context"/>
-        /// object we can call methods on that context object directly for a non-allocating approach. See
-        /// https://github.com/KyryloKuzyk/PrimeTween?tab=readme-ov-file#zero-allocations-with-delegates for more
-        /// information.
-        /// </para>
+        /// <remarks>
+        /// When using a <paramref name="targetFilter"/> it is important to be aware of what does and doesn't allocate
+        /// memory on the heap when working with delegates. Use this method which includes a context object to properly
+        /// use an instance or static method without allocating memory. See this link for more info:
+        /// https://github.com/KyryloKuzyk/PrimeTween?tab=readme-ov-file#zero-allocations-with-delegates
+        /// </remarks>
         /// <example>
         /// <code>
         /// // an example in which the context is not passed in. this results in a delegate allocation!
@@ -202,7 +209,7 @@ namespace Cyclic.FlexTargeting
         /// <param name="context">The context object to pass along into the <paramref name="targetFilter"/> in
         ///     order to avoid allocating heap memory with delegates. Typically this parameter is simply the calling
         ///     object: <c>this</c></param>
-        /// <param name="data">A container with the data and scoring method to use for sorting and ultimately
+        /// <param name="data">A structure with the data and scoring method to use for sorting and ultimately
         ///     determining the best target to return.</param>
         /// <param name="bestTarget">The best target that scores the best among the rest of the potential targets.
         ///     Will be null when no valid target was found.</param>
@@ -262,6 +269,7 @@ namespace Cyclic.FlexTargeting
 
         #region DetermineBestTargets
 
+        /// <inheritdoc cref="DetermineBestTargets{TTarget}(IFlexData{TTarget}, ICollection{TTarget}, IReadOnlyList{TTarget}, TargetFilter{TTarget})" />
         public static int DetermineBestTargets<TTarget>([NotNull] IFlexData<TTarget> data,
             ICollection<TTarget> bestTargets)
             where TTarget : class, IFlexTarget
@@ -272,6 +280,7 @@ namespace Cyclic.FlexTargeting
                 s_nullFilter);
         }
 
+        /// <inheritdoc cref="DetermineBestTargets{TTarget}(IFlexData{TTarget}, ICollection{TTarget}, IReadOnlyList{TTarget}, TargetFilter{TTarget})" />
         public static int DetermineBestTargets<TTarget>([NotNull] IFlexData<TTarget> data,
             ICollection<TTarget> bestTargets,
             IReadOnlyList<TTarget> inputTargets)
@@ -283,6 +292,7 @@ namespace Cyclic.FlexTargeting
                 s_nullFilter);
         }
 
+        /// <inheritdoc cref="DetermineBestTargets{TTarget}(IFlexData{TTarget}, ICollection{TTarget}, IReadOnlyList{TTarget}, TargetFilter{TTarget})" />
         public static int DetermineBestTargets<TTarget>([NotNull] IFlexData<TTarget> data,
             ICollection<TTarget> bestTargets,
             [NotNull] TargetFilter<TTarget> targetFilter)
@@ -294,6 +304,32 @@ namespace Cyclic.FlexTargeting
                 targetFilter);
         }
 
+        /// <summary>
+        /// <para>
+        /// Filters and scores the targets in the <paramref name="inputTargets"/> list and outputs the passing targets
+        /// to the <paramref name="bestTargets"/> collection. The data and scoring method that is used to determine the
+        /// best targets is contained within the provided <paramref name="data"/> structure. Every target that passes
+        /// all conditions will be returned in the order of best scoring to worst. An optional
+        /// <paramref name="targetFilter"/> can be provided to further filter out valid targets.
+        /// </para>
+        /// <remarks>
+        /// When using a <paramref name="targetFilter"/> it is important to be aware of what does and doesn't allocate
+        /// memory on the heap when working with delegates. Use the overload of this method that includes a context
+        /// object to properly use an instance or static method without allocating memory. See this link for more info:
+        /// https://github.com/KyryloKuzyk/PrimeTween?tab=readme-ov-file#zero-allocations-with-delegates
+        /// </remarks>
+        /// </summary>
+        /// <param name="data">A structure with the data and scoring method to use for sorting and ultimately
+        ///     determining the best targets to return</param>
+        /// <param name="bestTargets">The collection that will contain the targets that passed all conditions. These
+        ///     targets will be added to the collection in the order of best scoring to worst.</param>
+        /// <param name="inputTargets">A list of targets that will be filtered, scored, and sorted to determine which
+        ///     are the best targets to return.</param>
+        /// <param name="targetFilter">A delegate that can further process each input target to determine whether it
+        ///     should be considered as a valid target to return.</param>
+        /// <typeparam name="TTarget">The type of flex target to be considered.</typeparam>
+        /// <returns>Returns the number of targets that were added to the <paramref name="bestTargets"/> collection.
+        /// </returns>
         public static int DetermineBestTargets<TTarget>([NotNull] IFlexData<TTarget> data,
             ICollection<TTarget> bestTargets,
             IReadOnlyList<TTarget> inputTargets,
@@ -323,18 +359,21 @@ namespace Cyclic.FlexTargeting
             s_potentialTargets.Sort(data.ScoreComparer);
 
             // send the filtered targets to the final targets list
+            int numTargetsAdded = 0;
             foreach (var targetItem in s_potentialTargets)
             {
                 bestTargets.Add(targetItem.flexTarget as TTarget);
+                numTargetsAdded++;
             }
 
-            return bestTargets.Count;
+            return numTargetsAdded;
         }
 
         #endregion
 
         #region DetermineBestTargets with context
 
+        /// <inheritdoc cref="DetermineBestTargets{TContext, TTarget}(TContext, IFlexData{TTarget}, ICollection{TTarget}, IReadOnlyList{TTarget}, TargetFilter{TContext, TTarget})" />
         public static int DetermineBestTargets<TContext, TTarget>(TContext context,
             [NotNull] IFlexData<TTarget> data,
             ICollection<TTarget> bestTargets,
@@ -348,6 +387,46 @@ namespace Cyclic.FlexTargeting
                 targetFilter);
         }
 
+        /// <summary>
+        /// <para>
+        /// Filters and scores the targets in the <paramref name="inputTargets"/> list and outputs the passing targets
+        /// to the <paramref name="bestTargets"/> collection. The data and scoring method that is used to determine the
+        /// best targets is contained within the provided <paramref name="data"/> structure. Every target that passes
+        /// all conditions will be returned in the order of best scoring to worst. An optional
+        /// <paramref name="targetFilter"/> can be provided to further filter out valid targets.
+        /// </para>
+        /// <remarks>
+        /// When using a <paramref name="targetFilter"/> it is important to be aware of what does and doesn't allocate
+        /// memory on the heap when working with delegates. Use this method which includes a context object to properly
+        /// use an instance or static method without allocating memory. See this link for more info:
+        /// https://github.com/KyryloKuzyk/PrimeTween?tab=readme-ov-file#zero-allocations-with-delegates
+        /// </remarks>
+        /// <example>
+        /// An example of how to utilize context:
+        /// <code>
+        /// // context is not passed in resulting in a delegate allocation every function call!
+        /// FlexTargetingCore.DetermineBestTargets(data, bestTargets, inputTargets, (target) => FilterMethod(target));
+        /// // an example in which the context IS passed in. this results in no allocation.
+        /// FlexTargetingCore.DetermineBestTargets(context: this, data, bestTargets, inputTargets, (context, target) => context.FilterMethod(target));
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="context">The context object to pass along into the <paramref name="targetFilter"/> in
+        ///     order to avoid allocating heap memory with delegates. Typically this parameter is simply the calling
+        ///     object: <c>this</c></param>
+        /// <param name="data">A structure with the data and scoring method to use for sorting and ultimately
+        ///     determining the best target to return.</param>
+        /// <param name="bestTargets">The collection that will contain the targets that passed all conditions. These
+        ///     targets will be added to the collection in the order of best scoring to worst.</param>
+        /// <param name="inputTargets">A list of targets that will be filtered, scored, and sorted to determine which
+        ///     are the best targets to return.</param>
+        /// <param name="targetFilter">A delegate that can further process each input target to determine whether it
+        ///     should be considered as a valid target to return.</param>
+        /// <typeparam name="TContext">The type of the <paramref name="context"/> object passed in which will be
+        /// passed along into the <paramref name="targetFilter"/></typeparam>
+        /// <typeparam name="TTarget">The type of flex target to be considered.</typeparam>
+        /// <returns>Returns the number of targets that were added to the <paramref name="bestTargets"/> collection.
+        /// </returns>
         public static int DetermineBestTargets<TContext, TTarget>(TContext context,
             [NotNull] IFlexData<TTarget> data,
             ICollection<TTarget> bestTargets,
@@ -378,12 +457,14 @@ namespace Cyclic.FlexTargeting
             s_potentialTargets.Sort(data.ScoreComparer);
 
             // send the filtered targets to the final targets list
+            int numTargetsAdded = 0;
             foreach (var targetItem in s_potentialTargets)
             {
                 bestTargets.Add(targetItem.flexTarget as TTarget);
+                numTargetsAdded++;
             }
 
-            return bestTargets.Count;
+            return numTargetsAdded;
         }
 
         #endregion
