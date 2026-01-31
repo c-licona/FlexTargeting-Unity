f611ad84fb7b2ea1bfd2e228d24d1158e096b693
clicona <46828592+c-licona@users.noreply.github.com>
2026-01-31 15:10:10 -0800

    Changes to FlexTargetComponent for automatic registration
    - Changed the RegisterWhen setup to be a property rather than a serialized and editable field. Every target type typically has its registration defined in the class itself. It doesn't make sense to make this setting public facing for editing on the editor side. Inheritors of this class can choose to make this registration setting public facing if they want
    - Updated some documentation

--- Runtime/FlexTargetComponent.cs
+++ Runtime/FlexTargetComponent.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 namespace Cyclic.FlexTargeting
 {
     /// <summary>
-    /// A generic MonoBehavior version of a <see cref="IFlexTarget"/> that will automatically register/unregister
-    /// itself to the <see cref="FlexTargetRepository"/> on enable/disable.
+    /// A generic MonoBehavior version of a <see cref="IFlexTarget"/> that can automatically register/unregister
+    /// itself to the <see cref="FlexTargetRepository"/> on awake/destroy, enable/disable or manually.
     /// </summary>
     /// <typeparam name="TSelf">This type should be the derived type itself so that it can be properly stored
     /// in the <see cref="FlexTargetRepository"/></typeparam>
@@ -17,6 +17,12 @@ namespace Cyclic.FlexTargeting
     /// </example>
     public abstract class FlexTargetComponent<TSelf> : MonoBehaviour, IFlexTarget where TSelf : FlexTargetComponent<TSelf>
     {
+        /// <summary>
+        /// The <see cref="FlexTargetComponent{TSelf}"/> takes care of some common boilerplate for
+        /// <see cref="IFlexTarget"/>s such as automatically registering this target to the
+        /// <see cref="FlexTargetRepository"/> on awake/destroy or enable/disable. Alternatively, this can be set to
+        /// manual to skip automatic registration.
+        /// </summary>
         protected enum RegisterWhen
         {
             [Tooltip("This target gets registered to the repository on Awake, and unregistered on Destroy.")]
@@ -27,13 +33,14 @@ namespace Cyclic.FlexTargeting
             Manual
         }
 
-        [Header("Flex Target Settings")]
-        [Tooltip("Set if and when this target automatically registers itself with the target repository.")]
-        [SerializeField] protected RegisterWhen _registerWhen = RegisterWhen.OnAwake;
+        /// <summary>
+        /// Determine when this target component registers itself with the <see cref="FlexTargetRepository"/>
+        /// </summary>
+        protected virtual RegisterWhen RegisterTargetWhen => RegisterWhen.OnAwake;
 
         protected virtual void Awake()
         {
-            if (_registerWhen == RegisterWhen.OnAwake)
+            if (RegisterTargetWhen == RegisterWhen.OnAwake)
             {
                 FlexTargetRepository.AddTarget(this as TSelf);
             }
@@ -41,7 +48,7 @@ namespace Cyclic.FlexTargeting
 
         protected virtual void OnDestroy()
         {
-            if (_registerWhen == RegisterWhen.OnAwake)
+            if (RegisterTargetWhen == RegisterWhen.OnAwake)
             {
                 FlexTargetRepository.RemoveTarget(this as TSelf);
             }
@@ -49,7 +56,7 @@ namespace Cyclic.FlexTargeting
 
         protected virtual void OnEnable()
         {
-            if (_registerWhen == RegisterWhen.OnEnable)
+            if (RegisterTargetWhen == RegisterWhen.OnEnable)
             {
                 FlexTargetRepository.AddTarget(this as TSelf);
             }
@@ -57,12 +64,14 @@ namespace Cyclic.FlexTargeting
 
         protected virtual void OnDisable()
         {
-            if (_registerWhen == RegisterWhen.OnEnable)
+            if (RegisterTargetWhen == RegisterWhen.OnEnable)
             {
                 FlexTargetRepository.RemoveTarget(this as TSelf);
             }
         }
 
+        // the default implementation of RegisterTargetWhen is to register on awake. So the most common default
+        // implementation of this method is to simply return if this target is currently enabled.
         public virtual bool IsTargetValid => enabled;
         public abstract Vector3 TargetPosition { get; }
         public abstract float LosBufferRadius { get; }
